fd40fbf8379db41ce129d9a6b8f9de7fb29cc411
selguedd <selguedd@ulb.ac.be>
2018-10-25 17:25:13 +0200

    Update game.h

---
+++ game.h
@@ -0,0 +1,18 @@
+#ifndef GAME_H
+#define GAME_H
+#include "mainwindow.h"
+class Game{
+public:
+    static Game& Instance();
+    void move(QMouseEvent *e);
+    void InitGame(MainWindow &wind);
+    Gameobject getgameobject(int x,int y);
+private:
+    Game();
+    static Game gameinst;
+    MainWindow *window;
+    Gameobject gameobject[21][21];
+    int posXselec=0;
+    int posYselec=0;
+};
+#endif // GAME_H
