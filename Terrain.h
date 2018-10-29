92dc653d16a31d9b279c61b317b508300c5a4581
selguedd <selguedd@ulb.ac.be>
2018-10-29 18:58:06 +0100

    Update Terrain.h

--- Terrain.h
+++ Terrain.h
@@ -1,12 +1,18 @@
 #ifndef TERRAIN_H
 #define TERRAIN_H
 #include "Gameobject.h"
+#include <string>
+using namespace std;
 
 class Terrain : public Gameobject {
 public:
+    string getFilename() const;
 
+protected:
+    string filename;
 private:
     int nbVie;
+
 
 };
 
