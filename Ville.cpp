861ce3d9e5d375b094b053d438a1b98e42d8fea9
selguedd <selguedd@ulb.ac.be>
2018-11-01 14:05:29 +0100

    Update Ville.cpp

--- Ville.cpp
+++ Ville.cpp
@@ -6,3 +6,29 @@ Ville::Ville(int X, int Y)
     posY = Y;
     type = 0;
 }
+
+int Ville::getCapturepoint() const
+{
+    return capturepoint;
+}
+
+void Ville::setCapturepoint(int value)
+{
+    capturepoint = value;/*if capturepoint =0 setteam*/
+
+}
+
+void Ville::repair(Unites c)
+{
+
+}
+
+int Ville::getTeam() const
+{
+    return team;
+}
+
+void Ville::setTeam(int value)
+{
+    team = value;
+}
