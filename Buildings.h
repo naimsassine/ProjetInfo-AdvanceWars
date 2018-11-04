2e13b2283b9d7836f6388dd41149bb5fed4cdd1b
selguedd <selguedd@ulb.ac.be>
2018-11-04 02:03:09 +0100

    Update Buildings.h

--- Buildings.h
+++ Buildings.h
@@ -1,14 +1,17 @@
 #ifndef BUILDINGS_H
 #define BUILDINGS_H
 #include "Terrain.h"
-
+#include "Unites.h"
 class Buildings : public Terrain {
 
 public:
     Buildings();
     Buildings(int X, int Y);
 
-
+    //virtual void repair(Unites c);
+    //virtual int getTeam() const;
+    //virtual void setTeam(int value);
+   // virtual ~Buildings();
 };
 
 
