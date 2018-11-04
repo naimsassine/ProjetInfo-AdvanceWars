afc86563cca2bd19d9b3c6c042a61ffec1b1d090
selguedd <selguedd@ulb.ac.be>
2018-11-04 02:02:47 +0100

    Update Usine.h

--- Usine.h
+++ Usine.h
@@ -1,11 +1,21 @@
 #ifndef USINE_H
 #define USINE_H
 #include "Buildings.h"
-
+#include "Unites.h"
 class Usine : public Buildings
 {
 public:
-    Usine();
+    Usine(int posX, int posY, int def, int argent, int PC);
+    Usine(int X, int Y);
+    void repair(Unites c);
+    int getTeam() const;
+    void setTeam(int value);
+    void createTeam(Unites c);
+
+private:
+    int capturepoint=20;
+    int team=0;
+    /*team =1 red team =2 blue*/
 };
 
 #endif // USINE_H
