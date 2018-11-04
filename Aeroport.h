fa9d6bafdd5651bc70c15ee5d1b4564f6a00c7b1
selguedd <selguedd@ulb.ac.be>
2018-11-04 02:03:17 +0100

    Update Aeroport.h

--- Aeroport.h
+++ Aeroport.h
@@ -1,11 +1,14 @@
 #ifndef AEROPORT_H
 #define AEROPORT_H
 #include "Buildings.h"
-
+#include"Aerienne.h"
 class Aeroport : public Buildings
 {
 public:
     Aeroport(int X, int Y);
+    void repair(Aerienne a);
+    int getTeam() const;
+    void setTeam(int value);
 };
 
 #endif // AEROPORT_H
