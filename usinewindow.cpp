c79b691d06497fb2668ec5c09f00b8664b3f17cf
selguedd <selguedd@ulb.ac.be>
2018-11-10 19:20:47 +0100

    Update usinewindow.cpp

---
+++ usinewindow.cpp
@@ -0,0 +1,50 @@
+#include "usinewindow.h"
+#include "ui_usinewindow.h"
+#include "game.h"
+UsineWindow::UsineWindow(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::UsineWindow)
+{
+    ui->setupUi(this);
+
+    Game& game=Game::Instance();
+    if(game.getTurn()==1){
+    QPixmap pix(":/osinfantry.gif");
+    ui->label->setPixmap(pix.scaled(50,50));}
+    else{
+        QPixmap pix(":/blue.png");
+        ui->label->setPixmap(pix.scaled(50,50));
+    }
+
+}
+
+UsineWindow::~UsineWindow()
+{
+    delete ui;
+}
+
+void UsineWindow::on_pushButton_clicked()
+{
+    Game& game=Game::Instance();
+    game.createUnite(x,y,game.getTurn());
+}
+
+int UsineWindow::getY() const
+{
+    return y;
+}
+
+void UsineWindow::setY(int value)
+{
+    y = value;
+}
+
+int UsineWindow::getX() const
+{
+    return x;
+}
+
+void UsineWindow::setX(int value)
+{
+    x = value;
+}
