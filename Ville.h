d132aa2dd56ca08b76117b7c36d16a1e60245575
selguedd <selguedd@ulb.ac.be>
2018-11-10 16:57:27 +0100

    Update Ville.h

--- Ville.h
+++ Ville.h
@@ -6,17 +6,22 @@
 class Ville : public Buildings
 {
 public:
-    Ville(int X, int Y);
+    Ville(int X, int Y ,int Z=20,int T=0);
     int getCapturepoint() const;
     void setCapturepoint(int value);
     void repair(Unites c);
     int getTeam() const;
     void setTeam(int value);
+    void initTeam();
+    void initCapturePoint();
+    bool getUnitin() const;
+    void setUnitin(bool value);
 
 private:
-    int capturepoint=20;
-    int team=0;
-    /*team =1 red team =2 blue*/
+    int capturepoint;
+    int team;
+    bool unitin=false;
+   /*team =1 red team =2 blue*/
 };
 
 #endif // VILLE_H
