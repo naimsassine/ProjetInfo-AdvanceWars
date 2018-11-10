1a8b9e97f482eb065445b485b476fc606f573575
selguedd <selguedd@ulb.ac.be>
2018-11-10 16:57:59 +0100

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,6 +11,8 @@
 #include <QPainter>
 #include <iostream>
 #include <QDockWidget>
+#include <QCoreApplication>
+#include <QSizePolicy>
 MainWindow::MainWindow(QWidget *parent) :
 
     QMainWindow(parent),
@@ -22,30 +24,44 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     map = new Map(this);
     MainWindow::setCentralWidget(map);
-
+    QSize size(1040,600);
+    this->setFixedSize(size);
  //Gestions des boutons sur la map//
 
 
-   bouton = new QPushButton("Quitter !", this);
-   bouton->setGeometry(60, 50, 180, 70);
-   bouton->move(1200, 50);
-   Game& game=Game::Instance();
 
 
-
-
-   //QObject::connect(bouton, SIGNAL(clicked()), qApp, SLOT(game.createUnite()));
+   bouton = new QPushButton("End Turn !", this);
+      bouton->setGeometry(60, 50, 180, 70);
+      bouton->move(10, 520);
+   QObject::connect(bouton, SIGNAL(clicked()), this, SLOT(changeturn()));
    bouton->show();
-   map->redraw();
+   lab = new QLabel("Player 1 ",this);
+   lab->setGeometry(60, 50, 180, 70);
+   QFont f( "Arial", 20, QFont::Bold);
+   lab->setFont( f);
+   lab->move(50, 50);
+   lab->show();
+   lab2= new QLabel("Money : ",this);
+   lab2->setGeometry(60, 50, 180, 70);
+   QFont f2( "Arial", 16);
+   lab2->setFont( f2);
+   lab2->move(0, 100);
+   lab2->show();
+   lab3= new QLabel("1000",this);
+   lab3->setGeometry(60, 50, 180, 70);
 
-
-
-
+   lab3->setFont( f2);
+   lab3->move(100, 100);
+   lab3->show();
 
 }
 
 MainWindow::~MainWindow()
-{
+{   delete bouton;
+    delete lab;
+    delete lab2;
+    delete lab3;
     delete ui;
     delete map;
 }
@@ -58,15 +74,26 @@ void MainWindow::InitMap()
 
 void MainWindow::redraw()
 {
-
+    map->setgameobject();
     map->redraw();
 }
 
-void MainWindow::on_pushButton_clicked()
+void MainWindow::changeturn()
 {
     Game& game=Game::Instance();
     game.endtour();
+    if(game.getTurn()==1){
+        lab->setText("Player 1");
+
+    }
+    else{
+        lab->setText("Player 2");
+    }
+    Player *player=game.getPlayer();
+    lab3->setText(QString::number(player->getMoney()));
 }
+
+
 
 
 
