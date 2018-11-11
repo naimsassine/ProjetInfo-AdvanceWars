30fcfff0d385a464caa5fbb6565a6205e4568673
Rami-Taheri <44472788+Rami-Taheri@users.noreply.github.com>
2018-11-11 11:33:14 +0100

    map

--- map.cpp
+++ map.cpp
@@ -335,15 +335,15 @@ void Map::mousePressEvent(QMouseEvent *ev)
             }**/
 
 
-
+//unites[i].getType()==1998 && unites[i].getTeam()==2
         }
-        else if(gameobject[z][e].getTeam() == 2){
+        else if(gameobject[z][e].getType() == 1998){
             Game& game=Game::Instance();
             std::vector<Unites> unite =game.getUnites();
             for(std::vector<Unites>::size_type i = 0; i != unite.size(); i++){
 
 
-                if(unite[i].getPosX()==z &&unite[i].getPosY()==e && unite[i].getUnitin()&& unite[i].getTeam()==0){
+                if(unite[i].getPosX()==z &&unite[i].getPosY()==e && unite[i].getUnitin() && unite[i].getTeam()==2){
                     QMenu menu(this);
                     attack =new QAction("Attaquer", this);
                     menu.addAction(attack);
