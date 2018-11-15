7a20bdeb7da83833fa8d0ee1d08411b60f10f671
selguedd <selguedd@ulb.ac.be>
2018-11-15 15:13:08 +0100

    Update map.cpp

--- map.cpp
+++ map.cpp
@@ -42,60 +42,116 @@ void Map::paintEvent(QPaintEvent *event)
            if(gameobject[i][j].getType() == 1){
                   QPixmap pixmap(":/plain.gif");
                   painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                  if(gameobject[i][j].getMovable()){
+                                 QPixmap pixmap(":/Misc.png");
+                                 painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                             }
            }
 
 
            else if(gameobject[i][j].getType() == 2){
                QPixmap pixmap(":/mountain.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 3){
                QPixmap pixmap(":/wood.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 4){
                QPixmap pixmap(":/hriver.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 5){
                QPixmap pixmap(":/vriver.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 6){
                QPixmap pixmap(":/criver.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 7){
                QPixmap pixmap(":/esriver.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 8){
                QPixmap pixmap(":/swriver.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 9){
                QPixmap pixmap(":/wnriver.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 104){
                QPixmap pixmap(":/espipe.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 102){
                QPixmap pixmap(":/hpipe.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 108){
                QPixmap pixmap(":/epipeend.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 18){
                QPixmap pixmap(":/esroad.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 15){
                QPixmap pixmap(":/hroad.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 34){
 
@@ -108,103 +164,204 @@ void Map::paintEvent(QPaintEvent *event)
                if(gameobject[i][j].getTeam()==2){
                QPixmap pixmap(":/bluemooncity.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);}
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 30){
                QPixmap pixmap(":/hshoal.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 33){
                QPixmap pixmap(":/reef.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                       if(gameobject[i][j].getMovable()){
+                                      QPixmap pixmap(":/Misc.png");
+                                      painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                                  }
            }
            else if(gameobject[i][j].getType() == 19){
                QPixmap pixmap(":/swroad.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 106){
                QPixmap pixmap(":/wnpipe.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 24){
                QPixmap pixmap(":/wneroad.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 22){
                QPixmap pixmap(":/eswroad.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 101){
                QPixmap pixmap(":/vpipe.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 125){
                QPixmap pixmap(":/jadesunhq.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 35){
                QPixmap pixmap(":/neutralbase.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 109){
                QPixmap pixmap(":/spipeend.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 123){
                QPixmap pixmap(":/jadesunbase.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 23){
                QPixmap pixmap(":/swnroad.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 21){
                QPixmap pixmap(":/neroad.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 16){
                QPixmap pixmap(":/vroad.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 26){
                QPixmap pixmap(":/hbridge.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 29){
                QPixmap pixmap(":/hshoal.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 20){
                QPixmap pixmap(":/wnroad.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 110){
                QPixmap pixmap(":/wpipeend.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 36){
                QPixmap pixmap(":/neutralairport.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 95){
                QPixmap pixmap(":/blackholehq.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 92){
                QPixmap pixmap(":/blackholebase.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 25){
                QPixmap pixmap(":/nesroad.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 10){
                QPixmap pixmap(":/neriver.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
            else if(gameobject[i][j].getType() == 107){
                QPixmap pixmap(":/npipeend.gif");
                painter.drawPixmap(c*40,j*40,40,40,pixmap);
+               if(gameobject[i][j].getMovable()){
+                              QPixmap pixmap(":/Misc.png");
+                              painter.drawPixmap(c*40,j*40,40,40,pixmap);
+                          }
            }
+
 
 
            else{
