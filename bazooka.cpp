348321fdf40903b87a2e7952215bc75907568bd5
selguedd <selguedd@ulb.ac.be>
2018-11-21 23:26:48 +0100

    Create bazooka.cpp

---
+++ bazooka.cpp
@@ -0,0 +1,11 @@
+#include "bazooka.h"
+
+Bazooka::Bazooka(int InitX, int InitY, int InitTeam)
+{
+    posX=InitX;
+    type=2003;
+    posY=InitY;
+    Team = InitTeam;
+    vie=10;
+    ptdeplacement = 6;
+}
