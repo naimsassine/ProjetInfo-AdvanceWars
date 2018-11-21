47bbd344b418a92c72b6b915cfb43a9b78679d9f
selguedd <selguedd@ulb.ac.be>
2018-11-21 23:26:57 +0100

    Update usinewindow.cpp

--- usinewindow.cpp
+++ usinewindow.cpp
@@ -11,10 +11,34 @@ UsineWindow::UsineWindow(QWidget *parent) :
     Game& game=Game::Instance();
     if(game.getTurn()==1){
     QPixmap pix(":/osinfantry.gif");
-    ui->label->setPixmap(pix.scaled(50,50));}
+    QPixmap pix1(":/osmech.gif");
+    QPixmap pix2(":/osrecon.gif");
+    QPixmap pix3(":/osanti-air.gif");
+    QPixmap pix4(":/ostank.gif");
+    QPixmap pix5(":/osmd.tank.gif");
+    ui->label->setPixmap(pix.scaled(30,30));
+    ui->label1->setPixmap(pix1.scaled(30,30));
+    ui->label2->setPixmap(pix2.scaled(30,30));
+    ui->label3->setPixmap(pix3.scaled(30,30));
+    ui->label4->setPixmap(pix4.scaled(30,30));
+    ui->label5->setPixmap(pix5.scaled(30,30));
+
+
+
+    }
     else{
-        QPixmap pix(":/blue.png");
-        ui->label->setPixmap(pix.scaled(50,50));
+        QPixmap pix(":/bminfantry.gif");
+        QPixmap pix1(":/bmmech.gif");
+        QPixmap pix2(":/bmrecon.gif");
+        QPixmap pix3(":/bmanti-air.gif");
+        QPixmap pix4(":/bmtank.gif");
+        QPixmap pix5(":/bmmd.tank.gif");
+        ui->label->setPixmap(pix.scaled(30,30));
+        ui->label1->setPixmap(pix1.scaled(30,30));
+        ui->label2->setPixmap(pix2.scaled(30,30));
+        ui->label3->setPixmap(pix3.scaled(30,30));
+        ui->label4->setPixmap(pix4.scaled(30,30));
+        ui->label5->setPixmap(pix5.scaled(30,30));
     }
 
 }
@@ -27,7 +51,7 @@ UsineWindow::~UsineWindow()
 void UsineWindow::on_pushButton_clicked()
 {
     Game& game=Game::Instance();
-    game.createUnite(x+1,y,game.getTurn());
+    game.createUnite(x+1,y,game.getTurn(),1998);
 
 }
 
@@ -49,4 +73,35 @@ int UsineWindow::getX() const
 void UsineWindow::setX(int value)
 {
     x = value;
+}
+
+void UsineWindow::on_pushButton_2_clicked()
+{
+    Game& game=Game::Instance();
+    game.createUnite(x+1,y,game.getTurn(),2003);
+}
+
+void UsineWindow::on_pushButton_3_clicked()
+{
+    Game& game=Game::Instance();
+    game.createUnite(x+1,y,game.getTurn(),3000);
+
+}
+
+void UsineWindow::on_pushButton_5_clicked()
+{
+    Game& game=Game::Instance();
+    game.createUnite(x+1,y,game.getTurn(),3002);
+}
+
+void UsineWindow::on_pushButton_6_clicked()
+{
+    Game& game=Game::Instance();
+    game.createUnite(x+1,y,game.getTurn(),3003);
+}
+
+void UsineWindow::on_pushButton_4_clicked()
+{ Game& game=Game::Instance();
+    game.createUnite(x+1,y,game.getTurn(),3001);
+
 }
