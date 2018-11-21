fb40cac78119e24b09955ff56e88f3c94c115b6a
selguedd <selguedd@ulb.ac.be>
2018-11-21 23:26:53 +0100

    Update usinewindow.h

--- usinewindow.h
+++ usinewindow.h
@@ -25,6 +25,16 @@ private slots:
     void on_pushButton_clicked();
 
 
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_6_clicked();
+
+    void on_pushButton_4_clicked();
+
 private:
     Ui::UsineWindow *ui;
     int x;
