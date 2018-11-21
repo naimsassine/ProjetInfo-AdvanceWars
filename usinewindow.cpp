7b7c446774ed871f37a32eb4eb239ba02936d21c
selguedd <selguedd@ulb.ac.be>
2018-11-21 23:57:34 +0100

    Update usinewindow.cpp

--- usinewindow.cpp
+++ usinewindow.cpp
@@ -16,12 +16,22 @@ UsineWindow::UsineWindow(QWidget *parent) :
     QPixmap pix3(":/osanti-air.gif");
     QPixmap pix4(":/ostank.gif");
     QPixmap pix5(":/osmd.tank.gif");
+    QPixmap pix6(":/osmegatank.gif");
+    QPixmap pix7(":/osneotank.gif");
+    QPixmap pix8(":/osb-copter.gif");
+    QPixmap pix9(":/osfighter.gif");
+    QPixmap pix10(":/osbomber.gif");
     ui->label->setPixmap(pix.scaled(30,30));
     ui->label1->setPixmap(pix1.scaled(30,30));
     ui->label2->setPixmap(pix2.scaled(30,30));
     ui->label3->setPixmap(pix3.scaled(30,30));
     ui->label4->setPixmap(pix4.scaled(30,30));
     ui->label5->setPixmap(pix5.scaled(30,30));
+    ui->label6->setPixmap(pix6.scaled(30,30));
+    ui->label7->setPixmap(pix7.scaled(30,30));
+    ui->label8->setPixmap(pix8.scaled(30,30));
+    ui->label9->setPixmap(pix9.scaled(30,30));
+    ui->label10->setPixmap(pix10.scaled(30,30));
 
 
 
@@ -33,12 +43,22 @@ UsineWindow::UsineWindow(QWidget *parent) :
         QPixmap pix3(":/bmanti-air.gif");
         QPixmap pix4(":/bmtank.gif");
         QPixmap pix5(":/bmmd.tank.gif");
+        QPixmap pix6(":/bmmegatank.gif");
+        QPixmap pix7(":/bmneotank.gif");
+        QPixmap pix8(":/bmb-copter.gif");
+        QPixmap pix9(":/bmfighter.gif");
+        QPixmap pix10(":/bmbomber.gif");
         ui->label->setPixmap(pix.scaled(30,30));
         ui->label1->setPixmap(pix1.scaled(30,30));
         ui->label2->setPixmap(pix2.scaled(30,30));
         ui->label3->setPixmap(pix3.scaled(30,30));
         ui->label4->setPixmap(pix4.scaled(30,30));
         ui->label5->setPixmap(pix5.scaled(30,30));
+        ui->label6->setPixmap(pix6.scaled(30,30));
+        ui->label7->setPixmap(pix7.scaled(30,30));
+        ui->label8->setPixmap(pix8.scaled(30,30));
+        ui->label9->setPixmap(pix9.scaled(30,30));
+        ui->label10->setPixmap(pix10.scaled(30,30));
     }
 
 }
@@ -104,4 +124,34 @@ void UsineWindow::on_pushButton_4_clicked()
 { Game& game=Game::Instance();
     game.createUnite(x+1,y,game.getTurn(),3001);
 
+}
+
+void UsineWindow::on_pushButton_7_clicked()
+{
+    Game& game=Game::Instance();
+        game.createUnite(x+1,y,game.getTurn(),3004);
+}
+
+void UsineWindow::on_pushButton_8_clicked()
+{
+    Game& game=Game::Instance();
+        game.createUnite(x+1,y,game.getTurn(),3005);
+}
+
+void UsineWindow::on_pushButton_9_clicked()
+{Game& game=Game::Instance();
+    game.createUnite(x+1,y,game.getTurn(),2000);
+
+}
+
+void UsineWindow::on_pushButton_10_clicked()
+{
+    Game& game=Game::Instance();
+        game.createUnite(x+1,y,game.getTurn(),2002);
+}
+
+void UsineWindow::on_pushButton_11_clicked()
+{
+    Game& game=Game::Instance();
+        game.createUnite(x+1,y,game.getTurn(),2001);
 }
