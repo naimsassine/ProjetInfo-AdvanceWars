db2e16e7019dbdb588e5b3c64d05d4b7d60e8b99
selguedd <selguedd@ulb.ac.be>
2018-11-30 22:25:52 +0100

    Create menu.cpp

---
+++ menu.cpp
@@ -0,0 +1,44 @@
+#include "menu.h"
+#include "ui_menu.h"
+#include "mainwindow.h"
+#include"Gameobject.h"
+#include "game.h"
+#include <QApplication>
+#include <QHBoxLayout>
+#include "player.h"
+Menu::Menu(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::Menu)
+{
+    ui->setupUi(this);
+}
+
+Menu::~Menu()
+{
+    delete ui;
+}
+
+void Menu::on_pushButton_clicked()
+{
+    this->close();
+
+    window->show();
+
+
+
+
+
+
+
+}
+
+MainWindow *Menu::getWindow() const
+{
+    return window;
+}
+
+void Menu::setWindow(MainWindow *value)
+{
+    window = value;
+}
+
