74b8c009b55bbb974f4e96246ac935b275320621
selguedd <selguedd@ulb.ac.be>
2018-11-30 22:25:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,21 +4,24 @@
 #include <QApplication>
 #include <QHBoxLayout>
 #include "player.h"
+#include "menu.h"
 int main(int argc, char *argv[])
 {
 
 
     QApplication a(argc, argv);
+    Menu menu;
+
     MainWindow w;
     Player player1(1000,1);
     Player player2(1000,2);
     Game& game=Game::Instance();
     game.InitGame(w,player1,player2);
-    w.show();
+    menu.setWindow(&w);
 
     int x = 5;
     int y = 6;
-
+    menu.show();
 
     return a.exec();
 
