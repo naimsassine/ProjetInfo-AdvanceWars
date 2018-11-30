1fa7d8f3664e798ea4c2feb45de2de8cb988de82
selguedd <selguedd@ulb.ac.be>
2018-11-30 22:25:55 +0100

    Create menu.h

---
+++ menu.h
@@ -0,0 +1,33 @@
+#ifndef MENU_H
+#define MENU_H
+#include "mainwindow.h"
+#include <QMainWindow>
+
+namespace Ui {
+class Menu;
+}
+
+class Menu : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit Menu(QWidget *parent = nullptr);
+    ~Menu();
+
+
+
+
+    MainWindow *getWindow() const;
+    void setWindow(MainWindow *value);
+
+private slots:
+    void on_pushButton_clicked();
+
+
+private:
+    Ui::Menu *ui;
+    MainWindow *window;
+};
+
+#endif // MENU_H
