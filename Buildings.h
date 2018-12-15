dfd2f2a945fd43124b577ef2f88b0bb263e7ef0f
selguedd <selguedd@ulb.ac.be>
2018-12-15 18:17:43 +0100

    Update Buildings.h

--- Buildings.h
+++ Buildings.h
@@ -13,6 +13,18 @@ public:
     //virtual int getTeam() const;
     //virtual void setTeam(int value);
    // virtual ~Buildings();
+    int getUnite() const;
+    void setUnite(int value);
+    bool getUnitin() const;
+    void setUnitin(bool value);
+
+    int getCapturepoint() const;
+    void setCapturepoint(int value);
+
+protected :
+    int unite;
+    bool unitin=false;
+    int capturepoint;
 };
 
 
